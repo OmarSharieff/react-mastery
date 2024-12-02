7362be62021d908692756a9d476fff44fc40e5ee
Omar <omarsharief642002@gmail.com>
2024-12-02 11:21:26 +0530

    import and export more

--- 02_JavaScript Refresher/03_JS Export Import/scripts/index.js
+++ 02_JavaScript Refresher/03_JS Export Import/scripts/index.js
@@ -1,3 +1,5 @@
 import { myString } from './utils.js';
+// You can also use an alias whilst importing
+// e.g., import { myString as str } from './utils.js';
 
 console.log(myString);
--- 02_JavaScript Refresher/03_JS Export Import/scripts/utils.js
+++ 02_JavaScript Refresher/03_JS Export Import/scripts/utils.js
@@ -1,1 +1,5 @@
 export let myString = "Hello from utils.js!";
+
+// we can also export it like this
+// export default "Hello from utils.js";
+// NOTE: Only one default can be exported in a file.
