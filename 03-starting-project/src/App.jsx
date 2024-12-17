fcdf3962ff10fb84314aeb0d5da8f2c057a1ffae
Omar <omarsharief642002@gmail.com>
2024-12-17 16:19:42 +0530

    outputting dynamc values in jsx

--- 03-starting-project/src/App.jsx
+++ 03-starting-project/src/App.jsx
@@ -1,14 +1,30 @@
+import reactImg from "./assets/react-core-concepts.png"
+
+const randomWords = ["Fundamental", "Crucial", "Core"];
+
+function getRandomIndex(max) {
+  return Math.floor(Math.random() * max)
+}
+
+function Header() {
+  let randomWord = randomWords[getRandomIndex(randomWords.length)]
+  
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {randomWord} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
