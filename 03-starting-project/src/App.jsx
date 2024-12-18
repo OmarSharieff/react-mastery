847e202977a015509ba6332d1a5d41eee55b6331
Omar <omarsharief642002@gmail.com>
2024-12-18 08:05:48 +0530

    alternative props syntax

--- 03-starting-project/src/App.jsx
+++ 03-starting-project/src/App.jsx
@@ -1,14 +1,25 @@
-import reactImg from "./assets/react-core-concepts.png"
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
 
 const randomWords = ["Fundamental", "Crucial", "Core"];
 
 function getRandomIndex(max) {
-  return Math.floor(Math.random() * max)
+  return Math.floor(Math.random() * max);
+}
+
+function CoreComponents({title, image, description}) {
+  return (
+    <li>
+      <img src={image} alt="logo" />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
 }
 
 function Header() {
-  let randomWord = randomWords[getRandomIndex(randomWords.length)]
-  
+  let randomWord = randomWords[getRandomIndex(randomWords.length)];
+
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
@@ -26,7 +37,16 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            {/*In the following 'CoreCompenents', we have used an alternative syntax for passing props which also works as a shorthand notation */}
+            <CoreComponents {...CORE_CONCEPTS[0]}/> 
+            <CoreComponents {...CORE_CONCEPTS[1]}/>
+            <CoreComponents {...CORE_CONCEPTS[2]}/>
+            <CoreComponents {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
