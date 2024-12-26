f912945a229376033e733a22274dcae86be06b35
Omar <omarsharief642002@gmail.com>
2024-12-26 17:47:10 +0530

    code formatting

--- 03-starting-project/src/components/CoreConcepts.jsx
+++ 03-starting-project/src/components/CoreConcepts.jsx
@@ -2,17 +2,19 @@ import CoreConcept from "./CoreConcept.jsx";
 import { CORE_CONCEPTS } from "../data.js";
 
 export default function CoreConcepts() {
-    return (
-        <section id="core-concepts">
-          <h2>Time to get started!</h2>
-          <ul>
-            {/*In the following 'CoreCompenents', we have used an alternative syntax for passing props which also works as a shorthand notation */}
-            {CORE_CONCEPTS.map(conceptItem=><CoreConcept key={conceptItem.title} {...conceptItem}/>)}
-            {/* <CoreComponents {...CORE_CONCEPTS[0]}/> 
+  return (
+    <section id="core-concepts">
+      <h2>Time to get started!</h2>
+      <ul>
+        {/*In the following 'CoreCompenents', we have used an alternative syntax for passing props which also works as a shorthand notation */}
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        ))}
+        {/* <CoreComponents {...CORE_CONCEPTS[0]}/> 
             <CoreComponents {...CORE_CONCEPTS[1]}/>
             <CoreComponents {...CORE_CONCEPTS[2]}/>
             <CoreComponents {...CORE_CONCEPTS[3]}/> */}
-          </ul>
-        </section>
-    )
-} 
+      </ul>
+    </section>
+  );
+}
