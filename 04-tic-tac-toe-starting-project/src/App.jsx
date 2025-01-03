96e0811062d4d554f1f59cdd577d1b453c2d933b
Omar <omarsharief642002@gmail.com>
2025-01-03 15:15:40 +0530

    player 1 and player 2 basic code

--- 04-tic-tac-toe-starting-project/src/App.jsx
+++ 04-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,18 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player-name">PLAYER 1</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">PLAYER 2</span>
+          <span className="player-symbol">O</span>
+        </li>
+      </ol>
+    </div>
+  );
 }
 
-export default App
+export default App;
